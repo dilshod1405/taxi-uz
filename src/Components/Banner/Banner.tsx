d806a0845521e66c3ac59e25b3a606ddcb49a5ff
Dilshod <111531709+dilshod1405@users.noreply.github.com>
2024-09-12 12:23:20 +0900

    Update Banner.tsx

--- src/Components/Banner/Banner.tsx
+++ src/Components/Banner/Banner.tsx
@@ -12,7 +12,7 @@ const Banner = () => {
                 <TypingEffect text="  Taksi haydovchilari uchun ajoyib yangilik !" speed={80}/>
                 <div className="line" data-aos="fade-left" data-aos-duration="1000"/>
                 <p data-aos="fade-up" data-aos-duration="3000">Endi siz yo'lovchilarni emas, balki yo'lovchilar sizni izlab kelishadi. Qanday deysizmi ? Ushbu havola orqali bizning <span>🤖 telegram botimizda</span> ro'yhatdan o'ting va biz sizga marshrutingizga mos yo'lovchilarni yetkazamiz...</p>
-                <Button className='bbtn fa-beat-fade' href='https://t.me/architect_developer' data-aos="fade-up" data-aos-duration="3000" variant="contained" style={{backgroundColor: '#FFCC00', marginTop: '20px', color: 'black'}}><TelegramIcon/> Ro'yhatdan o'tish</Button>
+                <Button className='bbtn fa-beat-fade' href='https://t.me/uztaxi_probot' data-aos="fade-up" data-aos-duration="3000" variant="contained" style={{backgroundColor: '#FFCC00', marginTop: '20px', color: 'black'}}><TelegramIcon/> Ro'yhatdan o'tish</Button>
             </div>
             <div className="right">
                 <img src="bot.png" alt="" className='car' data-aos='zoom-in'/>
